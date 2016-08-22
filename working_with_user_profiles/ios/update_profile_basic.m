3a2b00e76436332a2d9e64b6c6605d967f57a1b7
Jay Dalvi <jaydalvi@users.noreply.github.com>
2016-08-22 12:56:20 +0530

    Update update_profile_basic.m
    Adding photo key for user profile updates

--- working_with_user_profiles/ios/update_profile_basic.m
+++ working_with_user_profiles/ios/update_profile_basic.m
@@ -16,6 +16,7 @@ NSDictionary *profile = @{
     @"Married": @"Y",               // Can be either Y or N
     @"DOB": d,                      // Date of Birth. An NSDate object
     @"Age": @28,                    // Not required if DOB is set
+    @"Photo": @"www.foobar.com/image.jpeg",  // URL to the image
 
 // optional fields. controls whether the user will be sent email, push etc.
     @"MSG-email": @NO,              // Disable email notifications
